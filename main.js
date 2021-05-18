a32c4e1d5af79277fe8daee69a7bd27f3f2ee99d
Olle de Jong <olledejong@gmail.com>
2021-05-18 20:57:25 +0200

    Added introduction screen and data

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(() => {
 
-    console.log("JS is working.")
+    $.getJSON("assets/questions.json", function(json) {
+        console.log(json);
+    });
+
+    $("#start").on("click", () => {
+
+        $("#quiz-content").css("display", "grid")
+        $("#start, #introduction").fadeOut("fast")
+    })
 
 });
